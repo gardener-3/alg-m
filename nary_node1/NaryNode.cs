8542be7846e7d12b8ff4713f5a5858753fe407be
gardener-3 <gardener-3@gmail.com>
2025-06-03 15:43:26 +0200

    Add NaryNode impl.

---
+++ nary_node1/NaryNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nary_node1
+{
+    internal class NaryNode<T>(T value)
+    {
+        public T Value { get; set; } = value;
+
+        public List<NaryNode<T>> Children { get; set; } = new List<NaryNode<T>>();
+
+        public void AddChild(T value) {        
+            Children.Add(new NaryNode<T>(value));
+        }
+
+        public override string ToString()
+        {
+            // my_value: child1_value child2_value child3_value
+            
+            var sb = new StringBuilder();
+            sb.Append($"{Value}: ");
+            foreach (var child in Children)
+            {
+                sb.Append($" {child.Value} ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ nary_node1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace nary_node1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new NaryNode<string>("Root");
+            root.AddChild("A");
+            root.AddChild("B");
+            root.AddChild("C");
+
+            var aNode = root.Children[0];
+            aNode.AddChild("D");
+            aNode.AddChild("E");
+
+            var cNode = root.Children[2];
+            cNode.AddChild("F");
+
+            var dNode = aNode.Children[0];
+            dNode.AddChild("G");
+
+            var fNode = cNode.Children[0];
+            fNode.AddChild("H");
+            fNode.AddChild("I");
+
+            // Print the immediate children of root
+            Console.WriteLine(root); // Root: A B C
+            Console.WriteLine(aNode); // A: D E
+            Console.WriteLine(root.Children[1]);
+            Console.WriteLine(cNode); // C: F
+            Console.WriteLine(dNode); // D: G
+            Console.WriteLine(aNode.Children[1]);
+            Console.WriteLine(fNode); // F: H I
+            Console.WriteLine(dNode.Children[0]);
+            Console.WriteLine(fNode.Children[0]);
+            Console.WriteLine(fNode.Children[1]);
+        }
+    }
+}
