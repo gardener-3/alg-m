c4c3a2e9aa3286eb7dd97695d4d1a6535217a78a
gardener-3 <gardener-3@gmail.com>
2025-06-03 13:17:24 +0200

    Add project files.

---
+++ binary_node1/BinaryNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node1
+{
+    internal class BinaryNode<T>(T value)
+    {
+        public T Value { get; set; } = value;
+
+        public BinaryNode<T> LeftChild { get; set; } = null;
+
+        public BinaryNode<T> RightChild { get; set; } = null;
+
+        public void AddLeft(T value)
+        {
+            this.LeftChild = new BinaryNode<T>(value);
+        }
+        public void AddRight(T value)
+        {
+            this.RightChild = new BinaryNode<T>(value);
+        }
+
+        public override string ToString()
+        {
+            const string nullPlaceholder = "null"; 
+            // Expected format 
+            // my_value: left_child_value right_child_value
+            return $"{this.Value}: {(this.LeftChild==null? nullPlaceholder:this.LeftChild.Value)} {(this.RightChild == null ? nullPlaceholder : this.RightChild.Value)}";
+        }
+    }
+}
---
+++ binary_node1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace binary_node1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new BinaryNode<string>("Root");
+
+            // Level 1
+            root.AddLeft("A");
+            root.AddRight("B");
+
+            // Level 2
+            root.LeftChild.AddLeft("C");
+            root.LeftChild.AddRight("D");
+
+            root.RightChild.AddRight("E");
+
+            // Level 3
+            root.RightChild.RightChild.AddLeft("F");
+
+            Console.WriteLine(root);                    // Root: A B
+            Console.WriteLine(root.LeftChild);          // A: C D
+            Console.WriteLine(root.RightChild);         // B: null E
+            Console.WriteLine(root.LeftChild.LeftChild);// C: null null
+            Console.WriteLine(root.LeftChild.RightChild);// D: null null
+            Console.WriteLine(root.RightChild.RightChild);// E: F null
+            Console.WriteLine(root.RightChild.RightChild.LeftChild);// F: null null
+
+        }
+    }
+}
